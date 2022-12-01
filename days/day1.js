5e972558c632044cf791b55d6a40b4360625062d
Michael Heinhold <michael.heinhold@gmail.com>
2022-12-01 12:41:17 -0700

    removed multiple parseInt

--- days/day1.js
+++ days/day1.js
@@ -3,7 +3,7 @@ const textFile = readFileSync('./inputs/day1.txt').toString('utf-8');
 const elves = textFile.split(/\r?\n\r?\n/);
 
 function solutions(text) {
-  const combinedCals = text.map(elf => parseInt(elf.split(/\r?\n/).reduce((a, b) => parseInt(a) + parseInt(b))));
+  const combinedCals = text.map(elf => elf.split(/\r?\n/).map(Number).reduce((a, b) => a + b));
   const largeToSmall = combinedCals.sort((a, b) => b - a)
   const threeLargestSums = largeToSmall.slice(0, 3).reduce((a, b) => a + b)
   console.log(largeToSmall[0])
