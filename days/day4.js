a7e980e626d404d9019e38b1a7c37bd126a3e8f1
Michael Heinhold <michael.heinhold@gmail.com>
2022-12-04 09:53:16 -0700

    DRY

--- days/day4.js
+++ days/day4.js
@@ -9,8 +9,7 @@ for(let i= 0; i < 100; i++) {
   numbers.push(i)
 }
 
-// PART 1
-function determineCompleteOverlaps(set1, set2) {
+function parseData(set1, set2) {
   const splitSet1 = set1.split('-').map(Number)
   const splitSet2 = set2.split('-').map(Number)
   let spreadSet1 = [];
@@ -21,22 +20,18 @@ function determineCompleteOverlaps(set1, set2) {
   for(let i = splitSet2[0]; i <= splitSet2[1]; i++){
     spreadSet2.push(i)
   }
+  determineCompleteOverlaps(spreadSet1, spreadSet2, splitSet1, splitSet2)
+  determineOverlaps(spreadSet1, spreadSet2, splitSet1, splitSet2)
+}
+
+// PART 1
+function determineCompleteOverlaps(spreadSet1, spreadSet2, splitSet1, splitSet2) {
   if(spreadSet1.includes(splitSet2[0]) && spreadSet1.includes(splitSet2[1])) completeOverlapCount++
   else if(spreadSet2.includes(splitSet1[0]) && spreadSet2.includes(splitSet1[1])) completeOverlapCount++
 }
 
 // PART 2
-function determineOverlaps(set1, set2) {
-  const splitSet1 = set1.split('-').map(Number)
-  const splitSet2 = set2.split('-').map(Number)
-  let spreadSet1 = [];
-  let spreadSet2 = [];
-  for(let i = splitSet1[0]; i <= splitSet1[1]; i++){
-    spreadSet1.push(i)
-  }
-  for(let i = splitSet2[0]; i <= splitSet2[1]; i++){
-    spreadSet2.push(i)
-  }
+function determineOverlaps(spreadSet1, spreadSet2, splitSet1, splitSet2) {
   if(spreadSet1.includes(splitSet2[0]) || spreadSet1.includes(splitSet2[1])) overlapCount++
   else if(spreadSet2.includes(splitSet1[0]) || spreadSet2.includes(splitSet1[1])) overlapCount++
 }
@@ -45,8 +40,7 @@ function splitAssignments(data){
   for(let set of data){
     const set1 = set.split(',')[0]
     const set2 = set.split(',')[1]
-    determineCompleteOverlaps(set1, set2)
-    determineOverlaps(set1, set2)
+    parseData(set1, set2)
   }
 }
 
